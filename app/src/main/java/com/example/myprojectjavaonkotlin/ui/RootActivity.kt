3ed203feb0aad42dc90c4d4013852a606710995a
Paul <paulik0182@gmail.com>
2022-10-11 11:47:32 +0300

    clean code

--- app/src/main/java/com/example/myprojectjavaonkotlin/ui/RootActivity.kt
+++ app/src/main/java/com/example/myprojectjavaonkotlin/ui/RootActivity.kt
@@ -1,14 +1,13 @@
-package com.example.myprojectjavaonkotlin;
+package com.example.myprojectjavaonkotlin.ui
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myprojectjavaonkotlin.R
 
-import android.os.Bundle;
+class RootActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
     }
 }
