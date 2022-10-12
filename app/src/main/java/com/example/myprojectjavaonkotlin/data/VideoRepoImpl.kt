cac0fbefb133f3ea6eabc5e620a169da1bb35e09
Paul <paulik0182@gmail.com>
2022-10-12 16:03:48 +0300

    Настройка MMVM (LiveData). Требует уточнения

--- app/src/main/java/com/example/myprojectjavaonkotlin/data/VideoRepoImpl.kt
+++ app/src/main/java/com/example/myprojectjavaonkotlin/data/VideoRepoImpl.kt
@@ -5,7 +5,7 @@ import com.example.myprojectjavaonkotlin.domain.repo.VideoRepo
 import java.lang.NullPointerException
 
 class VideoRepoImpl(
-): VideoRepo {
+) : VideoRepo {
 
     private var data: MutableList<VideoEntity> = mutableListOf()
 
@@ -15,18 +15,7 @@ class VideoRepoImpl(
     }
 
     override fun getVideo(onVideo: (MutableList<VideoEntity>) -> Unit) {
-        try {
-            addVideo(VideoEntity()).let {
-                onVideo.invoke(it)
-            }
-        } catch (exc: NullPointerException) {
-            exc.printStackTrace()
-        }
-    }
-
-    init {
-        data.add(VideoEntity
-            (1,
+        val video = VideoEntity(1,
             "Любовь и голуби",
             "Комедия",
             "1984 год",
@@ -35,7 +24,32 @@ class VideoRepoImpl(
                     "и вечная бывает не только в кино, но и они сами способны так любить. В центре " +
                     "сюжета будни деревенской семьи Василия Кузякина. Он живет простой обычной " +
                     "жизнью, есть у него и жена Надежда, женщина шумная и часто ругающая своего " +
-                    "мужа за увлечение голубями."))
+                    "мужа за увлечение голубями.")
+        try {
+            addVideo(video).let {
+                onVideo.invoke(it)
+            }
+        } catch (exc: NullPointerException) {
+            exc.printStackTrace()
+        }
+    }
+
+    init {
+        data.add(
+            VideoEntity
+                (
+                1,
+                "Любовь и голуби",
+                "Комедия",
+                "1984 год",
+                "Деревенская комедия - мелодрама «Любовь и голуби» повествует о непростых " +
+                        "взаимоотношениях простых людей, которые не думают о том, что любовь сильная " +
+                        "и вечная бывает не только в кино, но и они сами способны так любить. В центре " +
+                        "сюжета будни деревенской семьи Василия Кузякина. Он живет простой обычной " +
+                        "жизнью, есть у него и жена Надежда, женщина шумная и часто ругающая своего " +
+                        "мужа за увлечение голубями."
+            )
+        )
     }
 
 }
