ac1b10f8d61112abe6987b772690a7dc363754af
Paul <paulik0182@gmail.com>
2022-11-05 18:04:16 +0300

    Correction (Binding) -> VideoListAdapter

--- app/src/main/java/com/example/myprojectjavaonkotlin/ui/video/VideoListAdapter.kt
+++ app/src/main/java/com/example/myprojectjavaonkotlin/ui/video/VideoListAdapter.kt
@@ -1,27 +1,24 @@
 package com.example.myprojectjavaonkotlin.ui.video
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myprojectjavaonkotlin.R
 import com.example.myprojectjavaonkotlin.domain.entity.VideoEntity
 
 class VideoListAdapter(
     private var data: List<VideoEntity> = mutableListOf(),
-    private var onDetailVideoListener: (VideoEntity) -> Unit ={},
-) : RecyclerView.Adapter<VideoListViewHolder>(){
+    private var onDetailVideoListener: (VideoEntity) -> Unit = {},
+) : RecyclerView.Adapter<VideoListViewHolder>() {
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setData(video: List<VideoEntity>){
+    fun setData(video: List<VideoEntity>) {
         data = video
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoListViewHolder {
         return VideoListViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_video_list, parent, false),
+            parent,
             onDetailVideoListener
         )
     }
